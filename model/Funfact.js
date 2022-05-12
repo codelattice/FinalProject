0aee0a5c429656b7d1544c6ffda7a1aa756fb7b3
codelattice <89589147+codelattice@users.noreply.github.com>
2022-05-11 21:50:17 -0400

    Create Funfact.js

---
+++ model/Funfact.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const funfactSchema = new Schema({
+    stateCode: {
+        type: String,
+        required: true,
+        unique: true
+        },
+    funfacts: [{type: String}],
+});
+
+module.exports = mongoose.model('Funfact', funfactSchema);
